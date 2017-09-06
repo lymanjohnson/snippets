0ce8ef0d7d71afef10fc2af38295b5ea25534650
lymanjohnson <lyman.johnson@gmail.com>
2017-09-06 09:16:03 -0400

    Fresh attempt

--- app.js
+++ app.js
@@ -1,64 +1,165 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const mustacheExpress = require('mustache-express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const Snippet = require("./models/snippet");
-const snippetRouter = require("./routers/snippet");
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
-const models = require("./models/models");
-const flash = require('express-flash-messages'),
-const expressValidator = require('express-validator'),
-const User = models.User;
-
-const DUPLICATE_RECORD_ERROR = 11000;
-
-const mongoURL = 'mongodb://localhost:27017/snippets';
-mongoose.connect(mongoURL, {useMongoClient: true});
-mongoose.Promise = require('bluebird');
+const fs = require('fs'),
+    path = require('path'),
+    express = require('express'),
+    mustacheExpress = require('mustache-express'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    session = require('express-session'),
+    bodyParser = require('body-parser'),
+    models = require("./models"),
+    flash = require('express-flash-messages'),
+    mongoose = require('mongoose'),
+    expressValidator = require('express-validator'),
+    User = models.User;
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost/test');
 
 app.engine('mustache', mustacheExpress());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'mustache')
 app.set('layout', 'layout');
-
 app.use('/static', express.static('static'));
-app.use('',snippetRouter);
 
-module.exports = app;
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.authenticate(username, password, function(err, user) {
+            if (err) {
+                return done(err)
+            }
+            if (user) {
+                return done(null, user)
+            } else {
+                return done(null, false, {
+                    message: "There is no user with that username and password."
+                })
+            }
+        })
+    }));
 
-
-
-/* REDUNDANT ??
-app.get('/new/', function(req, res) {
-    res.render('new_snippet');
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
 });
 
-app.post('/new/', function(req, res) {
-    Snippet.create(req.body).then(function(snippet) {
-        res.redirect('/');
-    }).catch(function(error) {
-        let errorMsg;
-        if (error.code === DUPLICATE_RECORD_ERROR) {
-            // make message about duplicate
-            errorMsg = `The snippet name "${req.body.name}" has already been used.`
-        } else {
-            errorMsg = "You have encountered an unknown error."
-        }
-        res.render('new_snippet', {errorMsg: errorMsg});
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(expressValidator());
+
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new(require('express-sessions'))({
+        storage: 'mongodb',
+        instance: mongoose, // optional
+        host: 'localhost', // optional
+        port: 27017, // optional
+        db: 'test', // optional
+        collection: 'sessions', // optional
+        expire: 86400 // optional
     })
-});
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+})
 
 app.get('/', function(req, res) {
-    Snippet.find().then(function(snippets) {
-        res.render('index', {snippets: snippets});
-    })
+    res.render("index");
 })
-*/
+
+app.get('/login/', function(req, res) {
+    res.render("login", {
+        messages: res.locals.getMessages()
+    });
+});
+
+app.post('/login/', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login/',
+    failureFlash: true
+}))
+
+app.get('/register/', function(req, res) {
+    res.render('register');
+});
+
+app.post('/register/', function(req, res) {
+    req.checkBody('username', 'Username must be alphanumeric').isAlphanumeric();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+
+    req.getValidationResult()
+        .then(function(result) {
+            if (!result.isEmpty()) {
+                return res.render("register", {
+                    username: req.body.username,
+                    errors: result.mapped()
+                });
+            }
+            const user = new User({
+                username: req.body.username,
+                password: req.body.password
+            })
+
+            const error = user.validateSync();
+            if (error) {
+                return res.render("register", {
+                    errors: normalizeMongooseErrors(error.errors)
+                })
+            }
+
+            user.save(function(err) {
+                if (err) {
+                    return res.render("register", {
+                        messages: {
+                            error: ["That username is already taken."]
+                        }
+                    })
+                }
+                return res.redirect('/');
+            })
+        })
+});
+
+function normalizeMongooseErrors(errors) {
+    Object.keys(errors).forEach(function(key) {
+        errors[key].message = errors[key].msg;
+        errors[key].param = errors[key].path;
+    });
+}
+
+app.get('/logout/', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+const requireLogin = function (req, res, next) {
+  if (req.user) {
+    next()
+  } else {
+    res.redirect('/login/');
+  }
+}
+
+app.get('/secret/', requireLogin, function (req, res) {
+  res.render("secret");
+})
+
+app.listen(3000, function() {
+    console.log('Express running on http://localhost:3000/.')
+});
--- server.js
+++
@@ -1,5 +0,0 @@
-const app = require("./app");
-
-app.listen(3000, function () {
-    console.log('Express running on http://localhost:3000/.')
-});
