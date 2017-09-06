405d27a4330325e56b16366ce7261881cfcf256a
lymanjohnson <lyman.johnson@gmail.com>
2017-09-06 12:15:07 -0400

    Switching back to recipe schema

--- app.js
+++ app.js
@@ -156,8 +156,97 @@ const requireLogin = function (req, res, next) {
   }
 }
 
+const addIndexToIngredients = function(recipe) {
+    for (let idx = 0; idx < recipe.ingredients.length; idx++) {
+        recipe.ingredients[idx].index = idx;
+    }
+}
+
+const getRecipe = function(req, res, next) {
+    Recipe.findOne({_id: req.params.id}).then(function(recipe) {
+        req.recipe = recipe;
+        next();
+    })
+}
+
 app.get('/secret/', requireLogin, function (req, res) {
   res.render("secret");
+})
+
+app.use(getRecipe);
+
+app.get('/', function(req, res) {
+    const recipe = req.recipe;
+    recipe.findRecipesFromSameSource().then(function(otherRecipes) {
+        res.render("recipe", {
+            recipe: recipe,
+            recipesFromSameSource: otherRecipes
+        });
+    })
+})
+
+app.get('/edit/', function(req, res) {
+    const recipe = req.recipe;
+    console.log(JSON.stringify(recipe.getFormData()));
+    addIndexToIngredients(recipe);
+    res.render("edit_recipe", {
+        recipe: recipe,
+        fields: recipe.getFormData(),
+        nextIngIndex: recipe.ingredients.length
+    });
+})
+
+app.post("/edit/", function(req, res) {
+    const recipe = req.recipe;
+    recipe.name = req.body.name;
+    recipe.source = req.body.source;
+    recipe.prepTime = req.body.prepTime;
+    recipe.cookTime = req.body.cookTime;
+
+    const ingredients = (req.body.ingredients || []).filter(function(ingredient) {
+        return (ingredient.amount || ingredient.measure || ingredient.ingredient)
+    });
+
+    recipe.ingredients = ingredients;
+
+    const error = recipe.validateSync();
+
+    if (error) {
+        addIndexToIngredients(recipe);
+        console.log(error.errors);
+        res.render("edit_recipe", {
+            recipe: recipe,
+            fields: recipe.getFormData(),
+            nextIngIndex: recipe.ingredients.length,
+            errors: error.errors
+        });
+    } else {
+        recipe.save();
+        res.redirect(`/${recipe._id}/`);
+    }
+})
+
+app.get('/new_ingredient/', function(req, res) {
+    res.render("new_ingredient", {recipe: req.recipe});
+})
+
+app.post('/new_ingredient/', function(req, res) {
+    const recipe = req.recipe;
+    recipe.ingredients.push(req.body);
+    recipe.save().then(function() {
+        res.render("new_ingredient", {recipe: recipe});
+    })
+})
+
+app.get('/new_step/', function(req, res) {
+    res.render("new_step", {recipe: req.recipe});
+})
+
+app.post('/new_step/', function(req, res) {
+    recipe.steps.push(req.body.step);
+    recipe.save().then(function() {
+        res.render("new_step", {recipe: recipe});
+    })
 })
 
 
