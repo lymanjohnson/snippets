070df6ceb8a10f7d17fa7ddc55aad2a872181b75
lymanjohnson <lyman.johnson@gmail.com>
2017-09-07 22:27:27 -0400

    Beautify Pass

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const fs = require('fs'),
-    path = require('path'),
-    express = require('express'),
-    mustacheExpress = require('mustache-express'),
-    passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy,
-    session = require('express-session'),
-    bodyParser = require('body-parser'),
-    models = require("./models"),
-    flash = require('express-flash-messages'),
-    mongoose = require('mongoose'),
-    expressValidator = require('express-validator'),
-    User = models.User,
-    Snippet = models.Snippet,
-    Recipe = models.Recipe; //delete this later
+  path = require('path'),
+  express = require('express'),
+  mustacheExpress = require('mustache-express'),
+  passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy,
+  session = require('express-session'),
+  bodyParser = require('body-parser'),
+  models = require("./models"),
+  flash = require('express-flash-messages'),
+  mongoose = require('mongoose'),
+  expressValidator = require('express-validator'),
+  User = models.User,
+  Snippet = models.Snippet,
+  Recipe = models.Recipe; //delete this later
 
 const DUPLICATE_RECORD_ERROR = 11000;
 const app = express();
@@ -26,135 +26,135 @@ app.set('layout', 'layout');
 app.use('/static', express.static('static'));
 
 passport.use(new LocalStrategy(
-    function(username, password, done) {
-        User.authenticate(username, password, function(err, user) {
-            if (err) {
-                return done(err)
-            }
-            if (user) {
-                return done(null, user)
-            } else {
-                return done(null, false, {
-                    message: "There is no user with that username and password."
-                })
-            }
+  function(username, password, done) {
+    User.authenticate(username, password, function(err, user) {
+      if (err) {
+        return done(err)
+      }
+      if (user) {
+        return done(null, user)
+      } else {
+        return done(null, false, {
+          message: "There is no user with that username and password."
         })
-    }));
+      }
+    })
+  }));
 
 passport.serializeUser(function(user, done) {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-        done(err, user);
-    });
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
 });
 
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 app.use(expressValidator());
 
 app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false,
-    store: new(require('express-sessions'))({
-        storage: 'mongodb',
-        instance: mongoose, // optional
-        host: 'localhost', // optional
-        port: 27017, // optional
-        db: 'test', // optional
-        collection: 'sessions', // optional
-        expire: 86400 // optional
-    })
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  store: new(require('express-sessions'))({
+    storage: 'mongodb',
+    instance: mongoose, // optional
+    host: 'localhost', // optional
+    port: 27017, // optional
+    db: 'test', // optional
+    collection: 'sessions', // optional
+    expire: 86400 // optional
+  })
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.locals.user = req.user;
   next();
 })
 
 app.get('/', function(req, res) {
-    res.render("index");
-    // console.log("req.user.username",req.user.username);
-    // console.log("res.locals.user.username",res.locals.user.username);
-    // console.log("req.passport.user.username",req.passport.user.username); DOESN'T WORK
+  res.render("index");
+  // console.log("req.user.username",req.user.username);
+  // console.log("res.locals.user.username",res.locals.user.username);
+  // console.log("req.passport.user.username",req.passport.user.username); DOESN'T WORK
 })
 
 app.get('/login/', function(req, res) {
-    res.render("login", {
-        messages: res.locals.getMessages()
-    });
+  res.render("login", {
+    messages: res.locals.getMessages()
+  });
 });
 
 app.post('/login/', passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login/',
-    failureFlash: true
+  successRedirect: '/',
+  failureRedirect: '/login/',
+  failureFlash: true
 }))
 
 app.get('/register/', function(req, res) {
-    res.render('register');
+  res.render('register');
 });
 
 app.post('/register/', function(req, res) {
-    req.checkBody('username', 'Username must be alphanumeric').isAlphanumeric();
-    req.checkBody('username', 'Username must be lowercase').isLowercase();
-    req.checkBody('username', 'Username is required').notEmpty();
-    req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('username', 'Username must be alphanumeric').isAlphanumeric();
+  req.checkBody('username', 'Username must be lowercase').isLowercase();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
 
-    req.getValidationResult()
-        .then(function(result) {
-            if (!result.isEmpty()) {
-                return res.render("register", {
-                    username: req.body.username,
-                    errors: result.mapped()
-                });
-            }
-            const user = new User({
-                username: req.body.username,
-                password: req.body.password
-            })
+  req.getValidationResult()
+    .then(function(result) {
+      if (!result.isEmpty()) {
+        return res.render("register", {
+          username: req.body.username,
+          errors: result.mapped()
+        });
+      }
+      const user = new User({
+        username: req.body.username,
+        password: req.body.password
+      })
 
-            const error = user.validateSync();
-            if (error) {
-                return res.render("register", {
-                    errors: normalizeMongooseErrors(error.errors)
-                })
-            }
-
-            user.save(function(err) {
-                if (err) {
-                    return res.render("register", {
-                        messages: {
-                            error: ["That username is already taken."]
-                        }
-                    })
-                }
-                return res.redirect('/');
-            })
+      const error = user.validateSync();
+      if (error) {
+        return res.render("register", {
+          errors: normalizeMongooseErrors(error.errors)
         })
+      }
+
+      user.save(function(err) {
+        if (err) {
+          return res.render("register", {
+            messages: {
+              error: ["That username is already taken."]
+            }
+          })
+        }
+        return res.redirect('/');
+      })
+    })
 });
 
 function normalizeMongooseErrors(errors) {
-    Object.keys(errors).forEach(function(key) {
-        errors[key].message = errors[key].msg;
-        errors[key].param = errors[key].path;
-    });
+  Object.keys(errors).forEach(function(key) {
+    errors[key].message = errors[key].msg;
+    errors[key].param = errors[key].path;
+  });
 }
 
 app.get('/logout/', function(req, res) {
-    req.logout();
-    res.redirect('/');
+  req.logout();
+  res.redirect('/');
 });
 
-const requireLogin = function (req, res, next) {
+const requireLogin = function(req, res, next) {
   if (req.user) {
     next()
   } else {
@@ -163,46 +163,66 @@ const requireLogin = function (req, res, next) {
 }
 
 const getSnippet = function(req, res, next) {
-    Snippet.findOne({_id: req.params.id}).then(function(recipe) {
-        req.snippet = snippet;
-        next();
-    })
+  Snippet.findOne({
+    _id: req.params.id
+  }).then(function(recipe) {
+    req.snippet = snippet;
+    next();
+  })
 }
 
 // app.use(requireLogin);
 
-app.get('/users/', requireLogin, function (req, res) {
+app.get('/users/', requireLogin, function(req, res) {
   Snippet.distinct("author").then(function(snippet) {
 
-    res.render("users",{snippet:snippet})
+    res.render("users", {
+      snippet: snippet
+    })
 
-  // User.find().then(function(user) {
+    // User.find().then(function(user) {
     // res.render("users",{otherUser:user}); // different key "otherUser" needed here to avoid conflict with header bar reference to current logged in "user"
 
   })
 })
 
-app.get('/tagsbyuser/:username',requireLogin,function(req,res) {
-  Snippet.find({author:req.params.username}).distinct("tags").then(function(snippet){
-    res.render("tags",{snippet:snippet,username:req.params.username})
+app.get('/tagsbyuser/:username', requireLogin, function(req, res) {
+  Snippet.find({
+    author: req.params.username
+  }).distinct("tags").then(function(snippet) {
+    res.render("tags", {
+      snippet: snippet,
+      username: req.params.username
+    })
   })
 })
 
-app.get('/languagesbyuser/:username',requireLogin,function(req,res) {
-  Snippet.find({author:req.params.username}).distinct("languages").then(function(snippet){
-    res.render("users",{snippet:snippet})
+app.get('/languagesbyuser/:username', requireLogin, function(req, res) {
+  Snippet.find({
+    author: req.params.username
+  }).distinct("language").then(function(snippet) {
+    res.render("languages", {
+      snippet: snippet,
+      username: req.params.username
+    })
   })
 })
 
-app.get('/snippets/', requireLogin, function (req, res) {
+app.get('/snippets/', requireLogin, function(req, res) {
   Snippet.find().then(function(snippet) {
-    res.render("many",{snippet:snippet});
+    res.render("many", {
+      snippet: snippet
+    });
   })
 })
 
-app.get('/snippets/tag/:tag', requireLogin, function (req, res) {
-  Snippet.find({tags: req.params.tag}).then(function(snippet){
-    res.render("many",{snippet:snippet})
+app.get('/snippets/tag/:tag', requireLogin, function(req, res) {
+  Snippet.find({
+    tags: req.params.tag
+  }).then(function(snippet) {
+    res.render("many", {
+      snippet: snippet
+    })
   })
 })
 
@@ -212,58 +232,84 @@ app.get('/snippets/tag/:tag', requireLogin, function (req, res) {
 // })
 
 
-app.get('/snippets/language/:language', requireLogin, function (req, res) {
-  Snippet.find({language : req.params.language}).then(function(snippet){
-    res.render("many",{snippet:snippet})
+app.get('/snippets/language/:language', requireLogin, function(req, res) {
+  Snippet.find({
+    language: req.params.language
+  }).then(function(snippet) {
+    res.render("many", {
+      snippet: snippet
+    })
   })
 })
 
-app.get('/snippets/user/:username', requireLogin, function (req, res) {
-  Snippet.find({author: req.params.username}).then(function(snippet) {
-    res.render("many",{snippet:snippet});
+app.get('/snippets/user/:username', requireLogin, function(req, res) {
+  Snippet.find({
+    author: req.params.username
+  }).then(function(snippet) {
+    res.render("many", {
+      snippet: snippet
+    });
   })
 })
 
-app.get('/snippets/user/:username/tag/:tag', requireLogin, function (req, res) {
-  Snippet.find({author: req.params.username , tags: req.params.tag}).then(function(snippet) {
-    res.render("many",{snippet:snippet});
+app.get('/snippets/user/:username/tag/:tag', requireLogin, function(req, res) {
+  Snippet.find({
+    author: req.params.username,
+    tags: req.params.tag
+  }).then(function(snippet) {
+    res.render("many", {
+      snippet: snippet
+    });
   })
 })
 
-app.get('/snippets/user/:username/language/:language', requireLogin, function (req, res) {
-  Snippet.find({author: req.params.username , language: req.params.language}).then(function(snippet) {
-    res.render("many",{snippet:snippet});
+app.get('/snippets/user/:username/language/:language', requireLogin, function(req, res) {
+  Snippet.find({
+    author: req.params.username,
+    language: req.params.language
+  }).then(function(snippet) {
+    res.render("many", {
+      snippet: snippet
+    });
   })
 })
 
-app.get('/snippets/id/:id', requireLogin, function (req, res) {
-  Snippet.findOne({_id : req.params.id}).then(function(snippet){
-    res.render("one",{snippet:snippet , starred : snippet.stars.includes(res.locals.user.username)})
+app.get('/snippets/id/:id', requireLogin, function(req, res) {
+  Snippet.findOne({
+    _id: req.params.id
+  }).then(function(snippet) {
+    res.render("one", {
+      snippet: snippet,
+      starred: snippet.stars.includes(res.locals.user.username)
+    })
   })
 })
 
-app.get('/create/', requireLogin, function (req, res) {
+app.get('/create/', requireLogin, function(req, res) {
   res.render("create");
 })
 
-app.post('/star/',requireLogin, function(req, res){
+app.post('/star/', requireLogin, function(req, res) {
   console.log(req.body.id);
-  Snippet.findOne({_id : req.body.id}).then(function(snippet){
+  Snippet.findOne({
+    _id: req.body.id
+  }).then(function(snippet) {
     // If the stars list already includes the user, it removes all instances from the stars list (there should be only one but this is bug proofing)
     console.log(res.locals.user.username);
     console.log(snippet.stars.includes(res.locals.user.username));
-    if(snippet.stars.includes(res.locals.user.username)){
+    if (snippet.stars.includes(res.locals.user.username)) {
       i = 0;
-      while (i<snippet.stars.length){
+      while (i < snippet.stars.length) {
         console.log(i);
-        if (snippet.stars[i] == res.locals.user.username){
-          snippet.stars.splice(i,1);
+        if (snippet.stars[i] == res.locals.user.username) {
+          snippet.stars.splice(i, 1);
+        } else {
+          i++
         }
-        else {i++}
       }
     }
     // Otherwise, it adds the user to the stars list
-    else{
+    else {
       snippet.stars.push(res.locals.user.username);
     }
 
@@ -272,22 +318,23 @@ app.post('/star/',requireLogin, function(req, res){
   })
 })
 
-app.post('/create/', function (req, res) {
+app.post('/create/', function(req, res) {
   console.log(req.body);
   req.body.author = res.locals.user.username;
   tagArray = req.body.tagsRaw.split(" ");
-  let i =0
-  while (i<tagArray.length) {
-    if (tagArray[i] == ""){
-      tagArray.splice(i,1);
+  let i = 0
+  while (i < tagArray.length) {
+    if (tagArray[i] == "") {
+      tagArray.splice(i, 1);
+    } else {
+      i++
     }
-    else{i++}
   }
 
   req.body.tags = tagArray;
 
   Snippet.create(req.body)
-    .then(function (snippet) {
+    .then(function(snippet) {
       console.log(snippet);
       res.redirect('/');
     })
@@ -310,5 +357,5 @@ app.post('/create/', function (req, res) {
 
 
 app.listen(3000, function() {
-    console.log('Express running on http://localhost:3000/.')
+  console.log('Express running on http://localhost:3000/.')
 });
--- models.js
+++ models.js
@@ -2,43 +2,43 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        unique: true,
-        lowercase: true,
-        required: true
-    },
-    passwordHash: {
-        type: String,
-        required: true
-    }
+  username: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  }
 });
 
 userSchema.virtual('password')
-    .get(function() {
-        return null
-    })
-    .set(function(value) {
-        const hash = bcrypt.hashSync(value, 8);
-        this.passwordHash = hash;
-    })
+  .get(function() {
+    return null
+  })
+  .set(function(value) {
+    const hash = bcrypt.hashSync(value, 8);
+    this.passwordHash = hash;
+  })
 
 userSchema.methods.authenticate = function(password) {
-    return bcrypt.compareSync(password, this.passwordHash);
+  return bcrypt.compareSync(password, this.passwordHash);
 }
 
 userSchema.statics.authenticate = function(username, password, done) {
-    this.findOne({
-        username: username
-    }, function(err, user) {
-        if (err) {
-            done(err, false)
-        } else if (user && user.authenticate(password)) {
-            done(null, user)
-        } else {
-            done(null, false)
-        }
-    })
+  this.findOne({
+    username: username
+  }, function(err, user) {
+    if (err) {
+      done(err, false)
+    } else if (user && user.authenticate(password)) {
+      done(null, user)
+    } else {
+      done(null, false)
+    }
+  })
 };
 
 const snippetSchema = new mongoose.Schema({
@@ -71,18 +71,18 @@ const snippetSchema = new mongoose.Schema({
 });
 
 snippetSchema
-.virtual('numberOfStars')
-.get(function () {
-  return this.stars.length;
-});
+  .virtual('numberOfStars')
+  .get(function() {
+    return this.stars.length;
+  });
 
 snippetSchema.methods.findSnippetsFromSameAuthor = function(callback) {
-    return this.model('Snippet').find({
-        author: this.author,
-        _id: {
-            $ne: this._id
-        }
-    }, callback);
+  return this.model('Snippet').find({
+    author: this.author,
+    _id: {
+      $ne: this._id
+    }
+  }, callback);
 }
 
 
@@ -94,6 +94,6 @@ const ModelName = mongoose.model('SingularizedCapitalizedCollectionName',modelSc
 */
 
 module.exports = {
-    User: User,
-    Snippet: Snippet
+  User: User,
+  Snippet: Snippet
 };
